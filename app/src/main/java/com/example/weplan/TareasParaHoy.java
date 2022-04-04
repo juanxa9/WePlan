e190ab9fdc269a34a89e04fa60433d27c8443ecf
misge1 <misge@alumni.uv.es>
2022-04-04 16:58:35 +0200

    mis cambios

---
+++ app/src/main/java/com/example/weplan/TareasParaHoy.java
@@ -0,0 +1,14 @@
+package com.example.weplan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TareasParaHoy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tareas_para_hoy);
+    }
+}
---
+++ app/src/main/java/com/example/weplan/TareasTotales.java
@@ -0,0 +1,14 @@
+package com.example.weplan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TareasTotales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tareas_totales);
+    }
+}
