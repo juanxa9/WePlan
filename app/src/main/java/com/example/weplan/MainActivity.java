ef6a3a38469630a50efb9d6198d5b5f43ee4c990
juanxa9 <juan278857990@gmail.com>
2022-05-04 18:42:52 +0200

    splash_screen hecho

--- app/src/main/java/com/example/weplan/MainActivity.java
+++ app/src/main/java/com/example/weplan/MainActivity.java
@@ -26,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.Theme_WePlan);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
